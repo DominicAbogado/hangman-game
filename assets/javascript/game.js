2f9dcbf6e61b78ee1c0fdd3b4c88765132528899
DominicAbogado <dominic.j.abogado@gmail.com>
2018-04-16 20:10:01 -0400

    inner skeleton of hangman

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Array of names
+var gotnames = ["Tyrion","The Hound", "Daenerys", "Jon Snow", "Rhaegar", "Lannister", "Targaryen"];
+document.getElementById("demo").innerHTML = gotnames;
+
+
+// Choose name Randomly
+
+//Create Underscore based on word in array
+
+// get users guess
+
+//check if guess is right
+
+//if right, push to right array
+
+//if wrong, push to wrong array
